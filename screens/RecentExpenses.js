9bc4199fbee0e4ba91db6cee6599be320c81f4c0
IvanGrabovsky <ivan.grabovsky.ua@gmail.com>
2026-05-18 10:21:24 +0300

    Integrate Firebase backend for expense CRUD with form UI

---
+++ screens/RecentExpenses.js
@@ -0,0 +1,55 @@
+import { useContext, useEffect, useState } from "react";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { getDateMinusDays } from "../utils/date";
+import { fetchExpenses } from "../utils/http";
+import { ExpensesContext } from "../store/expenses-context";
+import Loader from "../components/UI/Loader";
+import ErrorOverlay from "../components/UI/ErrorOverlay";
+
+function RecentExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState();
+
+  const expensesContext = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      setIsFetching(true);
+
+      try {
+        const expenses = await fetchExpenses();
+        expensesContext.setExpenses(expenses);
+      } catch (error) {
+        setError("Could not fetch expenses!");
+      }
+      setIsFetching(false);
+    }
+
+    getExpenses();
+  }, []);
+
+  if (error && !isFetching) {
+    return <ErrorOverlay message={error} />;
+  }
+
+  if (isFetching) {
+    return <Loader />;
+  }
+
+  const recentExpenses = expensesContext.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date >= date7DaysAgo && expense.date <= today;
+  });
+
+  return (
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 days"
+      fallbackText="No expenses registered for the last 7 days!"
+    />
+  );
+}
+
+export default RecentExpenses;
